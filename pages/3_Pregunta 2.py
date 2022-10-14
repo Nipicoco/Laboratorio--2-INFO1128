9d43fa5ae0777a419615462e81d52e0da43af6e7
Nicolas V <nvergara2021@alu.uct.cl>
2022-10-14 06:13:40 -0300

    added comments

--- pages/3_Pregunta 2.py
+++ pages/3_Pregunta 2.py
@@ -12,32 +12,31 @@ st.sidebar.success("Seleccionar Pregunta")
 st.header('Ejercicio 2')
 st.image('2.png')
 codigo  = '''# Crops the image and saves it as "nombre_nuevo"
-def recortar(img, areas_recortes, nombre_nuevo):
-    imagenes_recortadas = img.crop(areas_recortes).resize((116, 116))
-    imagenes_recortadas.save(nombre_nuevo)
-def rotar(img, angulo, expandir, nuevos):
-    img_rotada = img.rotate(angulo, expand=expandir)
-    img_rotada.save(nuevos)    
+def recortar(img, areas_recortes, nombre_nuevo): # img es la imagen original, areas_recortes es una tupla con las coordenadas de la esquina superior izquierda y la esquina inferior derecha del recorte, nombre_nuevo es el nombre de la imagen recortada
+    imagenes_recortadas = img.crop(areas_recortes).resize((116, 116)) # recorta la imagen original y la redimensiona a 116x116
+    imagenes_recortadas.save(nombre_nuevo) # guarda la imagen recortada
+def rotar(img, angulo, expandir, nuevos): # img es la imagen original, angulo es el angulo de rotacion, expandir es un booleano que indica si la imagen se expande o no, nuevos es el nombre de la imagen rotada
+    img_rotada = img.rotate(angulo, expand=expandir) # rota la imagen original
+    img_rotada.save(nuevos)    # guarda la imagen rotada
+     
     
-    
-areas_recorte = [(10, 10, 210, 220), (242, 10, 484, 207), (505, 23, 723, 198), (733, 19, 980, 202)]
-angulo = [75, 100, 45, 120]
-# Una lista de tuplas. Cada tupla representa las coordenadas de la esquina superior izquierda y la
-# esquina inferior derecha del rectángulo que se va a recortar.
+areas_recorte = [(10, 10, 210, 220), (242, 10, 484, 207), (505, 23, 723, 198), (733, 19, 980, 202)] # tupla con las coordenadas de las esquinas de los recortes
+angulo = [75, 100, 45, 120] # lista con los angulos de rotacion
 
-figurras = 'figuras.png'
-plantillas = Image.open('plantilla.png')
-img = Image.open(figurras).convert('RGBA')
+
+figurras = 'figuras.png' # nombre de la imagen original
+plantillas = Image.open('plantilla.png') # abre la imagen original
+img = Image.open(figurras).convert('RGBA') # abre la imagen original y la convierte a RGBA
 
 # Iterando sobre la lista areas_recorte y guardando cada imagen recortada con un nombre diferente.
-for i, areas_recortes in enumerate(areas_recorte):
-    filename = os.path.splitext(figurras)[0]
-    ext = os.path.splitext(figurras)[1]
-    nombre_nuevo = filename + '_recortada' + str(i) + ext
+for i, areas_recortes in enumerate(areas_recorte): # i es el indice de la lista areas_recorte, areas_recortes es el elemento de la lista areas_recorte
+    filename = os.path.splitext(figurras)[0] # obtiene el nombre de la imagen original sin la extension
+    ext = os.path.splitext(figurras)[1] # obtiene la extension de la imagen original
+    nombre_nuevo = filename + '_recortada' + str(i) + ext # crea el nombre de la imagen recortada
 
-    recortar(img, areas_recortes, nombre_nuevo)
+    recortar(img, areas_recortes, nombre_nuevo) # llama a la funcion recortar
 
-# Rotando la imagen
+# Rotando la imagen 
 figura1 = Image.open('figuras_recortada0.png').rotate(75, expand=True)
 figura2 = Image.open('figuras_recortada1.png').rotate(100, expand=True)
 figura3 = Image.open('figuras_recortada2.png').rotate(45, expand=True)
--- pages/4_Pregunta 3.py
+++ pages/4_Pregunta 3.py
@@ -22,7 +22,7 @@ x = np.arange(start=1,stop=50,step=1) #la escala que vamos a usar
 val1,val2 = value(f1,x,1),value(f2,x,2) #creamos el arreglo de los coeficientes ya calculados con la funcion, x, y incremento 1
 plt.figure()
 plt.title("least square polynomial"),plt.xlabel('t(s)'), plt.ylabel('f(x)') #iniciamos y definimos atributos de la figura pyplot
-plt.plot(x,f1,'o'), plt.plot(x,f2,'o'), plt.plot(x,val1), plt.plot(x,val2)
+plt.plot(x,f1,'o'), plt.plot(x,f2,'o'), plt.plot(x,val1), plt.plot(x,val2) 
 #puntos de la 1        puntos de la 2       LSP de la 1     LSP de la 2
 plt.grid(),plt.show()'''
 cambio = ['Ocultar Código','Mostrar Código']
